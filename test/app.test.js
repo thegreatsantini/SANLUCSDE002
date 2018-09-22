7b06a3c57014757248079b3f7557af7f3e24843d
thegreatsantini <lss13@my.fsu.edu>
2018-09-21 22:18:30 -0700

    redone mark twain quotes fetching tests

--- test/app.test.js
+++ test/app.test.js
@@ -6,14 +6,14 @@ const fs = require('fs');
 const getQuotes = require('../goodReads').getQuotes;
 // const resolvePromise = require('../goodReads').resolvePromise;
 
-const GOODREADS_ENDPOINT =  process.env.GOODREADS_ENDPOINT
+const GOODREADS_ENDPOINT = process.env.GOODREADS_ENDPOINT
 
-const fetchData = fs.readFileSync('quotes.txt','utf8', () => {return 'read populated file'})
-const parsed = JSON.parse(fetchData)
+
 
 describe('Testing GoodReads Qoutes link', function () {
     it('should return 200 status', (done) => {
         request(GOODREADS_ENDPOINT, (err, res, body) => {
+            console.log(body)
             expect(res.statusCode).to.equal(200)
             done()
         })
@@ -26,32 +26,28 @@ describe('Testing GoodReads Qoutes link', function () {
     })
 });
 
-describe('Testing Quotes retreival functionality', () => {
-    it('clearing txt file', (done) => {
-        fs.truncate('quotes.txt', 0, () => {return 'truncated file'})
+describe('Testing data from qoutes.txt', () => {
+
+    const fetchData = fs.readFileSync('quotes.txt', 'utf8', () => { return 'read populated file' })
+    const parsed = JSON.parse(fetchData)
+
+    it('should be an array', (done) => {
+        expect(parsed).to.be.an('array')
         done()
     })
-    it('should be an empty text file before running', (done) => {
-        const data = fs.readFileSync('quotes.txt', () => {return 'read empty file'}).toString()
-        expect(data).to.be.empty
+    it('should be an array with 10 items', (done) => {
+        expect(parsed).to.have.lengthOf(10)
         done()
     })
-    it('should poplulate quotes.txt file after running', (done) => {
-        getQuotes()
-        const data = fs.readFileSync('quotes.txt','utf8', () => {return 'read populated file'})
-        console.log(JSON.parse(data))
-        // expect(JSON.parse(data)).to.have.lengthOf(10)
+    it('should have objects each with quotes, tags and votes keys', (done) => {
+        parsed.forEach(obj => {
+            expect(obj).to.have.keys(['quote', 'tags', 'votes']);
+        })
         done()
-    }).timeout(2000)
+    })
 })
 
-// describe('Testing quote retreival data', () => {
-//     it('should return an array', (done) => {
-//         const data = fs.readFileSync('quotes.txt', () => {return 'read populated file'}).toJSON()
-//         // console.log(data)
-//         done()
-//     }).timeout(2000)
-// })
+
 
 // describe('Facebook post scraper', function () {
 //     it('it sould not be empty', (done) => {
